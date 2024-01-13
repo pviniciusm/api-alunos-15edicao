25613ef0adb9eb31d9e460f5dc8a2cfa1313f011
Paulo <pvinics@gmail.com>
2024-01-12 22:20:11 -0300

    preparação para deploy no render

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 import { alunoRoutes } from "./routes/aluno.routes";
 import { loginRoutes } from "./routes/login.routes";
@@ -11,6 +13,6 @@ app.use(cors());
 app.use("/aluno", alunoRoutes());
 app.use("/login", loginRoutes());
 
-app.listen(3335, () => {
+app.listen(process.env.PORT, () => {
     console.log("API está rodando");
 });
